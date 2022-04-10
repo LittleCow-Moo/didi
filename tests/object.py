17181f4867711b31d44252e0479826bee41acc88
AWeirdScratcher <90096971+AWeirdScratcher@users.noreply.github.com>
2022-04-10 16:41:22 +0800

    object tests

---
+++ tests/object.py
@@ -0,0 +1,15 @@
+# basic object (MAIN)
+
+class Identify(object):
+  def __init__(self, user) -> None:
+    print("You have created a new object!")
+    self.user = user
+    
+  def show(self):
+    print(f"The username is {self.user['username']}, and the ID is {self.user['id']}")
+    
+UserObject = Identify({
+  "username": "AWeirdScratcher#1744",
+  "id": 123456789
+})
+UserObject.show()
